bd06e89735e8365a1f5a941733227de086d38649
Allen Rohner <arohner@gmail.com>
2014-08-26 14:29:59 -0500

    Move the client to a global for now. Remove more reflection warnings. Close the sink when the request is completed.

--- src/clj_ahttp/core.clj
+++ src/clj_ahttp/core.clj
@@ -1,12 +1,16 @@
 (ns clj-ahttp.core
   (:refer-clojure :exclude (get))
   (:require [clojure.string :as str]
-            [clojure.core.async :as a])
+            [clojure.core.async :as a]
+            [clojure.tools.logging :refer (infof errorf)])
   (:import (com.ning.http.client AsyncHttpClient
                                  AsyncHandler
                                  AsyncHandler$STATE
-                                 RequestBuilder)
-           (java.nio.channels Pipe)))
+                                 RequestBuilder
+                                 HttpResponseHeaders)
+           (java.nio ByteBuffer)
+           (java.nio.channels Pipe
+                              ReadableByteChannel)))
 
 (defn request-method->str [request-method]
   (-> request-method
@@ -38,6 +42,9 @@ options
               [k (first v)]))
        (into {})))
 
+
+(def client (AsyncHttpClient.))
+
 (defn request
   "Makes an async http request. Behaves like clj-http, except the return type is
 
@@ -50,8 +57,7 @@ A response is returned immediately, potentially even before the server has sent 
 The response map also contains a key, :abort!, a fn of no arguments. Call it to abort processing.
  "
   [{:keys [] :as args}]
-  (let [c (AsyncHttpClient.)
-        status (promise)
+  (let [status (promise)
         headers (promise)
         pipe (Pipe/open)
         source (.source pipe)
@@ -66,24 +72,43 @@ The response map also contains a key, :abort!, a fn of no arguments. Call it to 
               :body source
               :abort! (fn []
                         (swap! abort (constantly true)))}]
-    (.executeRequest c (build-request args) (reify AsyncHandler
-                                              (onThrowable [this throwable]
-                                                (throw throwable))
-                                              (onStatusReceived [this s]
-                                                (deliver status (.getStatusCode s))
-                                                (return-state))
-                                              (onHeadersReceived [this h]
-                                                (deliver headers (response-headers->map h))
-                                                (return-state))
-                                              (onBodyPartReceived [this body-part]
-                                                (let [buf (.getBodyByteBuffer body-part)
-                                                      buf-len (.remaining buf)
-                                                      written (.write sink buf)]
-                                                  (assert (= written buf-len)))
-                                                (return-state))
-                                              (onCompleted [this]
-                                                (.close sink))))
+    (.executeRequest ^AsyncHttpClient client (build-request args)
+                     (reify AsyncHandler
+                       (onThrowable [this throwable]
+                         (errorf throwable "error during request")
+                         (throw throwable))
+                       (onStatusReceived [this s]
+                         (deliver status (.getStatusCode s))
+                         (return-state))
+                       (onHeadersReceived [this h]
+                         (deliver headers (response-headers->map h))
+                         (return-state))
+                       (onBodyPartReceived [this body-part]
+                         (let [buf (.getBodyByteBuffer body-part)
+                               buf-len (.remaining buf)
+                               written (.write sink buf)]
+                           (assert (= written buf-len)))
+                         (return-state))
+                       (onCompleted [this]
+                         (.close sink))))
     resp))
+
+(defn drain-resp
+  "Convert to a normal clj-http-style response"
+  [resp]
+  (let [len (-> resp :headers deref (clojure.core/get "Content-Length") (#(Long/parseLong %)))
+        body-chan (-> resp :body)
+        buf (ByteBuffer/allocate (* 2 len))]
+    (loop []
+      (let [ret (.read ^ReadableByteChannel body-chan buf)]
+        (if (.hasRemaining buf)
+          (if (= ret -1)
+            (do
+              (.close body-chan)
+              (.flip buf)
+              buf)
+            (recur))
+          (throw (java.nio.BufferOverflowException "out of space")))))))
 
 (defn get [uri & [opts]]
   (request (merge opts
