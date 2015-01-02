0149b1bbd6feec3594f14861e819bda076705a20
Allen Rohner <arohner@gmail.com>
2015-01-02 14:06:06 -0600

    Specify a recently clojure version, so tests work properly

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :dependencies [[com.ning/async-http-client "1.8.13"]
                  [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]]
   :profiles {:dev {:dependencies [[clj-http "1.0.0"] ;; just to be able to compare responses
+                                  [org.clojure/clojure "1.7.0-alpha4"]
                                   [nio "1.0.3"]]}})
