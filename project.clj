d136bb7b28c9e8e5d0fcd7eda1819faf9a29a5e8
Allen Rohner <arohner@gmail.com>
2015-06-08 14:50:38 -0500

    Bump clojure

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :dependencies [[com.ning/async-http-client "1.9.23"]
                  [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]]
   :profiles {:dev {:dependencies [[clj-http "1.0.0"] ;; just to be able to compare responses
-                                  [org.clojure/clojure "1.7.0-alpha4"]
+                                  [org.clojure/clojure "1.7.0-RC1"]
                                   [nio "1.0.3"]]}})
