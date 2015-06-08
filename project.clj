9abdd0a4e5f1e8969b0a2ef67f4e31c21b91c4d7
Allen Rohner <arohner@gmail.com>
2015-06-08 15:09:35 -0500

    Fixes for `lein deploy`

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
                  [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]]
   :profiles {:dev {:dependencies [[clj-http "1.0.0"] ;; just to be able to compare responses
                                   [org.clojure/clojure "1.7.0-RC1"]
-                                  [nio "1.0.3"]]}})
+                                  [nio "1.0.3"]]}}
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :creds :gpg}]])
